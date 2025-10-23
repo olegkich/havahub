fd96765a0bb764c4ee5da2103b102b9787bb7b2e
olegkich8 <olegkich8@gmail..com>
2025-10-23 15:21:38 +0200

    fix path once again

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -204,7 +204,7 @@ const PORT = process.env.PORT || 3001;
 // Serve React build
 app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
 });
 
